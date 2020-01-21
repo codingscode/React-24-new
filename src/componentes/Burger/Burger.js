068c278fc589086e078bab53e905b7b736f8f199
Heronnes Pereira <heronnestg@hotmail.com>
2020-01-21 17:31:27 -0300

    parte 5

--- src/componentes/Burger/Burger.js
+++ src/componentes/Burger/Burger.js
@@ -4,11 +4,16 @@ import IngredientesBurger from './ingredientesBurger/ingredientesBurger'
 
 
 const burger = (props) => {
+  const transformIngredientes = Object.keys(props.ingredientes)
+     .map(igKey => {
+        return [...Array(props.ingredientes[igKey])].map((_, i) => {
+           return <IngredientesBurger key={igKey + i} type={igKey}/>
+        });
+     });
   return (
      <div className={classes.Burger}>
         <IngredientesBurger type="pao-topo" />
-        <IngredientesBurger type="queijo" />
-        <IngredientesBurger type="carne" />
+        {transformIngredientes}
         <IngredientesBurger type="pao-fundo" />
      </div>
   );
--- src/containers/BurgerConstrucao/burgerConstrutor.js
+++ src/containers/BurgerConstrucao/burgerConstrutor.js
@@ -5,10 +5,27 @@ import Burger from '../../componentes/Burger/Burger'
 
 
 class burgerConstrutor extends Component {
+
+    /*
+    constructor(props) {
+      super(props);
+      this.state = {...}
+    }
+    */
+
+    state = {
+      ingredientes: {
+        salada: 1,
+        bacon: 1,
+        queijo: 2,
+        carne: 2
+      }
+    }
+
     render() {
         return (
           <Auxiliar>
-             <Burger />
+             <Burger ingredientes={this.state.ingredientes}/>
              <div>Controle de Construção</div>
           </Auxiliar>
         )
