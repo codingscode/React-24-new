c3c870fdda9a75fc899765be05091142af4f7cd7
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-02 13:26:42 -0300

    parte 20

---
+++ src/componentes/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import burgerLogo from '../../assets/imagens/burger-logo.png'
+import classes from './Logo.css'
+
+const logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={burgerLogo} alt="MeuBurger"/>
+    </div>
+)
+
+export default logo;
--- src/componentes/Navegacao/BarrFerr/BarFerr.js
+++ src/componentes/Navegacao/BarrFerr/BarFerr.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import classes from './BarFerr.css'
+import Logo from '../../Logo/Logo'
 
 const barferr = (props) => (
     <header className={classes.BarFerr}>
         <div>MENU</div>
-        <div>LOGO</div>
+        <Logo />
         <nav>
             ...
         </nav>
