f24dd14a81a1f1f91e8d4a8ab1f8f5313fd7a4d7
Heronnes Pereira <heronnestg@hotmail.com>
2020-02-08 20:16:45 -0300

    parte 27

--- src/componentes/Navegacao/GavetaLateral/GavetaToggle/GavetaToggle.js
+++ src/componentes/Navegacao/GavetaLateral/GavetaToggle/GavetaToggle.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import classes from './GavetaToggle.css'
 
 const gavetaToggle = (props) => (
-    <div onClick={props.clicado}>MENU</div>
+    <div onClick={props.clicado} className={classes.GavetaToggle}>
+         <div></div>
+         <div></div>
+         <div></div>
+    </div>
 )
 
 export default gavetaToggle;
