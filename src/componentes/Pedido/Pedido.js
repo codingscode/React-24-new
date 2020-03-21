9e54c5cca1afb56ff396fe6c855a7c757e3fab05
Heronnes Pereira <heronnestg@hotmail.com>
2020-03-21 12:56:25 -0300

    parte 47

--- src/componentes/Pedido/Pedido.js
+++ src/componentes/Pedido/Pedido.js
@@ -2,11 +2,30 @@ import React from 'react'
 
 import classes from './Pedido.css'
 
-const pedido = (props) => (
-    <div className={classes.Pedido}>
-        <p>Ingredientes: Salada (1)</p>
-        <p>Preco: <strong>USD {Number.parseFloat(props.preco).toFixed(2)}</strong></p>
-    </div>
-)
+const pedido = (props) => {
+    const ingredientes = []
+
+    for (let nomeIngrediente in props.ingredientes) {
+        ingredientes.push({nome: nomeIngrediente, quantidade: props.ingredientes[nomeIngrediente]})
+    }
+
+    const resultadoIngrediente = ingredientes.map(ig => {
+        return <span key={ig.nome} style={{
+            textTransform: 'capitalize',
+            display: 'inline-block',
+            margin: '0 8px',
+            border: '1px solid #ccc',
+            padding: '5px'
+              }}>{ig.nome} ({ig.quantidade})</span>
+    })
+
+    return (
+      <div className={classes.Pedido}>
+         <p>Ingredientes: {resultadoIngrediente}</p>
+         <p>Preco: <strong>USD {Number.parseFloat(props.preco).toFixed(2)}</strong></p>
+      </div>
+    )
+    
+}
 
 export default pedido
