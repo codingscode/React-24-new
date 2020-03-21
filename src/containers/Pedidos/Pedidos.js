64256b768386e1a832b7e80154b71a8df02f7c0b
Heronnes Pereira <heronnestg@hotmail.com>
2020-03-21 12:12:43 -0300

    parte 45

--- src/containers/Pedidos/Pedidos.js
+++ src/containers/Pedidos/Pedidos.js
@@ -1,8 +1,32 @@
 import React, {Component} from 'react'
 
 import Pedido from '../../componentes/Pedido/Pedido'
+import axios from '../../axios-pedidos'
+import GerenComErro from '../../hoc/GerenComErro/GerenComErro'
 
 class Pedidos extends Component {
+    state = {
+        pedidos: [],
+        carregando: true
+     }
+     
+     componentDidMount() {
+        axios.get('/pedidos.json')
+           .then(res => {
+               const pedidosBuscados = []
+               for (let chave in res.data) {
+                  pedidosBuscados.push({
+                     ...res.data[chave],
+                     id: chave
+                  })
+               }
+               this.setState({carregando: false, pedidos: pedidosBuscados})         
+           })
+           .catch(erro => {
+               this.setState({carregando: false})
+           })
+     }
+
     render() {
         return (
             <div>
@@ -13,4 +37,4 @@ class Pedidos extends Component {
     }
 }
 
-export default Pedidos
+export default GerenComErro(Pedidos, axios)
