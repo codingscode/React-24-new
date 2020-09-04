2dfba7fe3a0f502228726fcace82d203314494e8
Heronnes Pereira <heronnestg@hotmail.com>
2020-09-03 21:41:15 -0300

    parte 71

---
+++ consertado/src/store/reducer.js
@@ -0,0 +1,15 @@
+import * as acaoTipos from './acoes'
+
+
+const stateInicial = {
+    ingredientes: null,
+    precoTotal: 4
+}
+ 
+const reducer = (state = stateInicial, acao) => {
+    
+}
+ 
+export default reducer
+
+
