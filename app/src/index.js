ddb021b20a9b3058cdf3bb2721520568b9f9b475
Heronnes Pereira <heronnestg@hotmail.com>
2021-02-01 11:43:12 -0300

    parte 79

--- app/src/index.js
+++ app/src/index.js
@@ -10,7 +10,7 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 import reducer from './store/reducer'
 
-const store = createStore(reducer)
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const app = (
     <Provider store={store}>
