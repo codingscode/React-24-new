a008595785f46d5fddead4e00dcf770ed084b107
Heronnes Pereira <heronnestg@hotmail.com>
2021-02-01 11:17:48 -0300

    parte 78

--- o_antigo_pasta024/src/axios-pedidos.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-
-const instancia = axios.create({
-    baseURL: 'https://react-meu-burger.firebaseio.com/'
-})
-
-export default instancia
--- o_antigo_pasta024/src/componentes/UI/Rodador/Rodador.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import classes from './Rodador'
-
-const rodador = () => (
-    <div className={classes.Carregador}>Carregando...</div>
-)
-
-export default rodador
